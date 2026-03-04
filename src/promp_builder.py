1fd67675f595385b1aaa35c227216b526c80c4dd
KikeMerino <palavix@gmail.com>
2026-03-04 09:55:09 +0100

    Prompt builder listo

--- src/promp_builder.py
+++ src/promp_builder.py
@@ -1,1 +1,12 @@
 # Construccion del prompt
+
+def build_prompt(context_chunks, user_query):
+    context = "\n\n".join(context_chunks)
+
+    prompt = f"""
+    Context: {context}
+
+    Question: {user_query}
+    """
+
+    return prompt
--- src/retriever.py
+++ src/retriever.py
@@ -1,1 +1,12 @@
 # Logica de retrieval (Top-k)
+# Dado un embeding de query, devolver los k chunks más relevantes de la colección
+
+from vectore_store import collection
+
+def retrieve(query_embedding, top_k=3):
+    results = collection.query(
+        query_embeddings=[query_embedding],
+        n_results=top_k
+    )
+    
+    return results['documents'][0], results['metadatas'][0]
